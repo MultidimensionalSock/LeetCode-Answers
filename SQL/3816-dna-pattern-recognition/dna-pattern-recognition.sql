5087670ca7775c26c9c8eefbc851f6258824fff6
Erithacus <92083124+MultidimensionalSock@users.noreply.github.com>
2026-09-12 13:51:12 +0100

    SQL

---
+++ SQL/3816-dna-pattern-recognition/dna-pattern-recognition.sql
@@ -0,0 +1,11 @@
+select sample_id, dna_sequence, species,
+    case when dna_sequence like 'ATG%' then 1 else 0 end as has_start,
+    case 
+        when dna_sequence like '%TAA' 
+        OR dna_sequence like '%TAG' 
+        OR dna_sequence like '%TGA' then 1 
+        else 0 
+        END AS has_stop,
+    case when dna_sequence like '%ATAT%' then 1 else 0 end as has_atat,
+    case when dna_sequence like '%GGG%' then 1 else 0 end as has_ggg
+from Samples
